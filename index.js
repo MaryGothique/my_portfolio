8c12a8845a4cad69477e92ced0c7ca806248f237
MaryGothique <marypolly88@gmail.com>
2024-07-05 21:32:07 +0200

    welcome and types

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-new TypeIt("#wellcome", {
-    strings: "wellcome!",
-    speed: 50,
-    waitUntilVisible: true,
-}).go();
+import TypeIt from "typeit";
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM fully loaded and parsed");
+
+  const navWelcome = document.querySelector("#nav-welcome");
+  const navProjects = document.querySelector("#nav-projects");
+  const navProfile = document.querySelector("#nav-profile");
+  const navReseauxSociaux = document.querySelector("#nav-reseaux-sociaux");
+
+  console.log(navWelcome, navProjects, navProfile, navReseauxSociaux);
+
+  if (navWelcome) {
+    new TypeIt("#nav-welcome", {
+      strings: "Welcome",
+      speed: 50,
+      waitUntilVisible: true,
+    }).go();
+  }
+
+  if (navProjects) {
+    new TypeIt("#nav-projects", {
+      strings: "Projects",
+      speed: 50,
+      waitUntilVisible: true,
+    }).go();
+  }
+
+  if (navProfile) {
+    new TypeIt("#nav-profile", {
+      strings: "Profile",
+      speed: 50,
+      waitUntilVisible: true,
+    }).go();
+  }
+
+  if (navReseauxSociaux) {
+    new TypeIt("#nav-reseaux-sociaux", {
+      strings: "Réseaux Sociaux",
+      speed: 50,
+      waitUntilVisible: true,
+    }).go();
+  }
+});
